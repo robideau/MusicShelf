f6429e408e79da499a5bc8d009373a1e60b89be2
David Robideau <robideau@iastate.edu>
2015-09-21 16:08:48 -0500

    Added alphabetization by title to library functions
    Added a basic function that sorts files by name into alphabetical
    containers held by the MusicLibrary object, written at the skill level
    of a high schooler who is really into python scripting. Please
    refactor/end the life of this code as soon as possible; it's an
    abomination. I just wanted to see if I could do it. Also ignore the fact
    that the only music present is The Strokes. The files are named
    conveniently.

--- Assets/Scripts/LibraryController.cs
+++ Assets/Scripts/LibraryController.cs
@@ -12,16 +12,21 @@ using UnityEngine.UI;
 public class LibraryController : MonoBehaviour {
 
 	string absolutePath = "./MusicLibraryDefault/";	//Default music library location
-	string[] fileTypes = {"ogg", "wav", "mp3"};	//Supported file types
+	//string[] fileTypes = {"ogg", "wav", "mp3"};	//Supported file types
 	FileInfo[] musicFiles; //holds all found music files
 	public UnityEngine.UI.InputField pointToDirectory; //Input field UI object
 	public GameObject pointToDirectoryObj; //Input field represented as active/inactive game object
 	bool filesLoaded = true; //If files have been searched/loaded since directory selected
+	public GameObject musicLibrary; //Contains a reference to the MusicLibrary container object
+	string[] startingChars = {"0-9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}; //jesus fucking christ just kill me right now
+	private GameObject[] alphabeticalTitles = new GameObject[27]; //Holds alphabetical-order-title file containers
 
 	int songIndex = 0;
 
 	// Use this for initialization
 	void Start () {
+
+		generateAlphabeticalTitleHolders(); //Generates "TitleX" containers, where X is each letter/number of the alphabet - move to update?
 
 	}
 	
@@ -60,11 +65,52 @@ public class LibraryController : MonoBehaviour {
 		DirectoryInfo directoryInfo = new DirectoryInfo (absolutePath); //open indicated directory
 		musicFiles = directoryInfo.GetFiles(); //add all files 
 
+		GameObject[] audioHolders = new GameObject[100]; //temporary placeholder max size
+		int currentTitleIndex = 1;
+
 		foreach(FileInfo f in musicFiles) {
-			print("Start loading "+f.FullName);
-			GameObject audioHolder = new GameObject("Audio file " + songIndex + " holder");
-			audioHolder.AddComponent<BoxCollider>();
+			print("Loading "+f.FullName);
+			GameObject audioHolder = new GameObject(f.Name);
+			audioHolders[songIndex] = audioHolder;
+			alphabetizeByTitle(audioHolder, currentTitleIndex);
+			//audioHolder.transform.parent = musicLibrary.transform;
+			currentTitleIndex++;
 			songIndex++;
+		}
+
+	}
+
+	void alphabetizeByTitle(GameObject currentAudioHolder, int currentTitleIndex) { //Sorts files by name - does not alphabetize beyond first letter
+
+		for (int i = 0; i < alphabeticalTitles.Length; i++) {
+
+			string firstChar = alphabeticalTitles.GetValue(i).ToString().Substring(5,1); 
+
+			if (currentAudioHolder.name.ToUpper().StartsWith(firstChar)) {
+
+				currentAudioHolder.transform.parent = alphabeticalTitles[i].transform;
+
+				return;
+
+			}
+
+
+		}
+
+		currentTitleIndex++;
+		return;
+	}
+
+	void generateAlphabeticalTitleHolders() {
+		int i = 0;
+		
+		foreach (string firstChar in startingChars) {
+			
+			GameObject currentFirstChar = new GameObject("Title" + firstChar.ToUpper());
+			alphabeticalTitles.SetValue(currentFirstChar, i);
+			i++;
+			currentFirstChar.transform.parent = musicLibrary.transform;
+			
 		}
 	}
 }
