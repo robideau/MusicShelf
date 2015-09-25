bf24aa933c6fe5bb22b06b697e895461a9ec4974
David Robideau <robideau@iastate.edu>
2015-09-25 16:23:13 -0500

    Added basic shelf generation script
    Still testing. Generates “shelves” to hold audio files. Allows easy
    alterations of shelf height, width, # per row, etc.

---
+++ Assets/Scripts/ShelfGenerator.cs
@@ -0,0 +1,45 @@
+﻿//David Robideau
+//
+//Generates meshes and necessary properties for the library "shelves"
+
+using UnityEngine;
+using System.Collections;
+
+public class ShelfGenerator : MonoBehaviour {
+
+	public int containerCount; //Number of "openable" containers that compose the shelf
+	public GameObject musicLibrary; //Holds a reference to the library
+	public float xPosInitial; //The starting position of the shelves - top left corner of top left shelf
+	public float yPosInitial; //Top left corner of top left shelf
+	public float zPos;
+	public float shelfWidth; //Width of each shelf
+	public float shelfHeight; //Heigh of each shelf
+	public int shelvesPerRow;
+	public bool assignLetters; //Determines whether or not to trigger the assignShelfLetters() function
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < containerCount/shelvesPerRow; i++) {
+			for (int j = 0; j < shelvesPerRow; j++) {
+				GameObject newShelf = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				newShelf.transform.position = new Vector3(xPosInitial + (shelfWidth * j), yPosInitial - (shelfHeight * i), zPos);
+				newShelf.transform.localScale = new Vector3(shelfWidth, shelfHeight, 1);
+			}
+		}
+
+
+		if (assignLetters) {
+			assignShelfLetters ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void assignShelfLetters() {
+
+	}
+}
